affb8b8c7b17226411e463134cfdb4d769610742
lennardevertz <levertz@idriss.xyz>
2023-12-11 17:27:00 +0100

    add blueprint, modify paths for server hosting

---
+++ streamer_blueprint.py
@@ -0,0 +1,27 @@
+from flask import Blueprint, render_template
+
+streamer_blueprint = Blueprint('streamer', __name__,
+                               template_folder='templates',
+                               static_folder='static',
+                               static_url_path='/frontends/static')
+
+
+@streamer_blueprint.route('/streamer')
+def submodule_index():
+    return render_template('streamerPage.html')
+
+
+@streamer_blueprint.route("/obs", methods=["GET"])
+def streamer_obs():
+    return render_template('obs.html')
+
+
+@streamer_blueprint.route("/donorPage", methods=["GET"])
+def streamer_donate():
+    return render_template('donorPage.html')
+
+
+@streamer_blueprint.route("/preview", methods=["GET"])
+def streamer_donate():
+    return render_template('donorPage.html')
+
