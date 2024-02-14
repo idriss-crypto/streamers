22b567293d0d07cd3de7fe5ef49a6bb834b70679
lennardevertz <levertz@idriss.xyz>
2024-02-14 19:40:18 +0100

    - add main.py for local deployment

---
+++ main.py
@@ -0,0 +1,29 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return 'Hello, World!'
+
+@app.route('/streamers')
+def submodule_index():
+    return render_template('streamerPage.html')
+
+
+@app.route("/streamers/obs", methods=["GET"])
+def streamer_obs():
+    return render_template('obs.html')
+
+
+@app.route("/streamers/donate", methods=["GET"])
+def streamer_donate():
+    return render_template('donorPage.html')
+
+
+@app.route("/streamers/preview", methods=["GET"])
+def obs_preview():
+    return render_template('obsPreview.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
