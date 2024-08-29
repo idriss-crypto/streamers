c6df96e3a02f5918634eaa3fccf2ae99358acb4b
lennardevertz <levertz@idriss.xyz>
2024-08-29 19:13:00 +0200

    - add footer note

--- main.py
+++ main.py
@@ -2,33 +2,36 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return 'Hello, World!'
 
-@app.route('/streamers')
+@app.route("/")
+def hello_world():
+    return "Hello, World!"
+
+
+@app.route("/streamers")
 def submodule_index():
-    return render_template('streamerPage.html')
+    return render_template("streamerPage.html")
 
 
 @app.route("/streamers/obs", methods=["GET"])
 def streamer_obs():
-    return render_template('obs.html')
+    return render_template("obs.html")
 
 
 @app.route("/streamers/donate", methods=["GET"])
 def streamer_donate():
-    return render_template('donorPage.html')
+    return render_template("donorPage.html")
 
 
 @app.route("/streamers/history", methods=["GET"])
 def streamer_history():
-    return render_template('tipHistory.html')
+    return render_template("tipHistory.html")
 
 
 @app.route("/streamers/preview", methods=["GET"])
 def obs_preview():
-    return render_template('obsPreview.html')
+    return render_template("obsPreview.html")
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     app.run(debug=True)
